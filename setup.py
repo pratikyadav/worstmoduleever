2af353d902cc3c5f4c3cc95a698b276ccaf2a826
Pratik Yadav <pratik@mapbox.com>
2019-06-11 16:35:46 -0700

    first pass

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='worstmoduleever',
+    version='0.1dev',
+    packages=['worstmoduleever', ],
+    license='MIT',
+    long_description=open('README.txt').read(),
+)
